08783eb4c5ff54f2e28434271600ecfc2e8b5e01
ddk976 <39350250+ddk976@users.noreply.github.com>
2020-12-09 20:29:41 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -70,7 +70,7 @@ FOREIGN KEY(OrderID) REFERENCES ORDERS(OrderID)  ON DELETE SET NULL ON UPDATE CA
 DROP FUNCTION  TotalOrderValue();
 DROP TABLE orderFigure;
 CREATE FUNCTION TotalOrderValue() 
-RETURNS TABLE (oid INTEGER,orderValue INTEGER) AS $orderFigure$
+RETURNS TABLE (oid INTEGER,orderValue NUMERIC(8,3)) AS $orderFigure$
 BEGIN 
    RETURN QUERY
     SELECT a.OrderID,SUM(a.total)
@@ -78,29 +78,48 @@ BEGIN
     GROUP BY a.OrderID;
 END $orderFigure$
 LANGUAGE plpgsql;
--- SELECT TotalOrderValue();
+--SELECT TotalOrderValue();
 DROP FUNCTION sellsFigure();
 DROP TABLE sellsFigure;
+-- CREATE FUNCTION sellsFigure()
+-- RETURNS TABLE (staffid INTEGER, totalsold NUMERIC(8,3))AS $soldFigure$
+-- BEGIN 
+--     --SELECT TotalOrderValue() AS orderFigure;
+    
+--     RETURN QUERY
+--     SELECT TotalOrderValue() AS orderFigure;
+--     SELECT a.StaffID,SUM(a.StaffID)
+--     FROM (SELECT * FROM STAFF_ORDERS INNER JOIN orderFigure ON orderFigure.oid=STAFF_ORDERS.OrderID)a
+--     GROUP BY a.SaffID;
+-- END $soldFigure$
+-- LANGUAGE plpgsql;
 CREATE FUNCTION sellsFigure()
-RETURNS TABLE (staffid INTEGER, totalsold INTEGER )AS $soldFigure$
+RETURNS TABLE (staffid INTEGER, totalsold BIGINT)AS $soldFigure$
 BEGIN 
-    --SELECT TotalOrderValue() AS orderFigure;
-    --SELECT TotalOrderValue() AS orderFigure;
     RETURN QUERY
     SELECT a.StaffID,SUM(a.StaffID)
-    FROM (SELECT * FROM STAFF_ORDERS INNER JOIN orderFigure ON orderFigure.oid=STAFF_ORDERS.OrderID)a
-    GROUP BY a.SaffID;
+    FROM (SELECT * FROM STAFF_ORDERS INNER JOIN (select * from TotalOrderValue()) AS f ON STAFF_ORDERS.OrderID=f.oid) a
+    GROUP BY a.StaffID;
 END $soldFigure$
 LANGUAGE plpgsql;
 DROP FUNCTION seller();
 DROP TABLE result;
+-- CREATE FUNCTION seller()
+-- RETURNS TABLE ( staffName VARCHAR(30), totalValue INTEGER) AS $result$
+-- BEGIN 
+--     RETURN QUERY 
+--     SELECT TotalOrderValue()AS soldFigure;
+--     SELECT a.name, a.totalsold
+--     FROM (SELECT totalsold,FName.STAFF+LName.STAFF AS name FROM STAFF INNER JOIN soldFigure ON soldFigure.staffid=STAFF.StaffID)a
+--     ORDER BY a.totalsold DESC;
+-- END $result$
+-- LANGUAGE plpgsql;
 CREATE FUNCTION seller()
-RETURNS TABLE ( staffName VARCHAR(30), totalValue INTEGER) AS $result$
+RETURNS TABLE ( fname VARCHAR(30), lname VARCHAR(30),totalValue BIGINT) AS $result$
 BEGIN 
     RETURN QUERY 
-    --SELECT TotalOrderValue()AS soldFigure;
-    SELECT a.name, a.totalsold
-    FROM (SELECT totalsold,FName.STAFF+LName.STAFF AS name FROM STAFF INNER JOIN soldFigure ON soldFigure.staffid=STAFF.StaffID)a
+    SELECT a.FName,a.LName, a.totalsold
+    FROM (SELECT STAFF.FName,STAFF.LName,totalsold FROM STAFF INNER JOIN (SELECT * FROM sellsFigure()) AS f ON STAFF.StaffID=f.staffid) a
     ORDER BY a.totalsold DESC;
 END $result$
 LANGUAGE plpgsql;
