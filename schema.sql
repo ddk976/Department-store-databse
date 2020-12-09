96cffa590af9456f59fc3325e9c78f430baba05f
ddk976 <39350250+ddk976@users.noreply.github.com>
2020-12-10 00:19:17 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -93,17 +93,18 @@ DROP TABLE sellsFigure;
 --     GROUP BY a.SaffID;
 -- END $soldFigure$
 -- LANGUAGE plpgsql;
-CREATE FUNCTION sellsFigure()
-RETURNS TABLE (staffid INTEGER, totalsold BIGINT)AS $soldFigure$
+CREATE FUNCTION sellsFigure(l INTEGER)
+RETURNS TABLE (staffid INTEGER, totalsold numeric(8,3))AS $soldFigure$
 BEGIN 
     RETURN QUERY
-    SELECT a.StaffID,SUM(a.StaffID)
+    SELECT a.StaffID,SUM(a.orderValue)
     FROM (SELECT * FROM STAFF_ORDERS INNER JOIN (select * from TotalOrderValue()) AS f ON STAFF_ORDERS.OrderID=f.oid) a
+    WHERE a.totalSold>l
     GROUP BY a.StaffID;
 END $soldFigure$
 LANGUAGE plpgsql;
 DROP FUNCTION seller();
-DROP TABLE result;
+
 -- CREATE FUNCTION seller()
 -- RETURNS TABLE ( staffName VARCHAR(30), totalValue INTEGER) AS $result$
 -- BEGIN 
@@ -115,11 +116,49 @@ DROP TABLE result;
 -- END $result$
 -- LANGUAGE plpgsql;
 CREATE FUNCTION seller()
-RETURNS TABLE ( fname VARCHAR(30), lname VARCHAR(30),totalValue BIGINT) AS $result$
+RETURNS TABLE ( fname VARCHAR(30), lname VARCHAR(30),totalValue numeric(8,3)) AS $result$
 BEGIN 
     RETURN QUERY 
-    SELECT a.FName,a.LName, a.totalsold
-    FROM (SELECT STAFF.FName,STAFF.LName,totalsold FROM STAFF INNER JOIN (SELECT * FROM sellsFigure()) AS f ON STAFF.StaffID=f.staffid) a
+    SELECT a.FName,a.LName, a.totalsol
+    FROM (SELECT STAFF.FName,STAFF.LName,totalsold FROM STAFF INNER JOIN 
+            (SELECT * FROM sellsFigure(50000)) AS f ON STAFF.StaffID=f.staffid) a
     ORDER BY a.totalsold DESC;
 END $result$
 LANGUAGE plpgsql;
+DROP FUNCTION op8();
+CREATE FUNCTION op8()
+RETURNS TABLE (pid INTEGER, oid INTEGER, sum numeric(8,3))AS $$
+BEGIN
+    RETURN QUERY
+    SELECT a.ProductID,a.OrderID,SUM(a.total)as sum
+    FROM (SELECT ORDER_PRODUCTS.OrderID,ORDER_PRODUCTS.productID,INVENTORY.ProductPrice*ORDER_PRODUCTS.ProductQuantity AS total FROM INVENTORY INNER JOIN ORDER_PRODUCTS ON INVENTORY.ProductID=ORDER_PRODUCTS.ProductID)a 
+    WHERE sum>20000
+    GROUP BY a.ProductID,a.OrderID
+    ORDER BY SUM(a.total) DESC;
+END $$
+LANGUAGE plpgsql;
+DROP FUNCTION id();
+CREATE FUNCTION id()
+RETURNS TABLE ( staffid INTEGER) AS $$
+BEGIN 
+    RETURN QUERY 
+    SELECT a.StaffID
+    FROM (SELECT s.StaffID FROM (SELECT STAFF_ORDERS.StaffID FROM (SELECT * FROM op8())AS m INNER JOIN 
+                STAFF_ORDERS  ON m.oid=STAFF_ORDERS.OrderID)s INNER JOIN (SELECT * FROM sellsFigure(30000))AS n ON s.StaffID=n.staffid)a;
+END $$
+LANGUAGE plpgsql;
+DROP FUNCTION name();
+CREATE FUNCTION name()
+RETURNS TABLE (fname VARCHAR(30), lname VARCHAR(30) )AS $$
+BEGIN   
+    RETURN QUERY
+    SELECT a.FName, a.LName
+    FROM(SELECT STAFF.FName,STAFF.LName FROM STAFF INNER JOIN 
+            (SELECT * FROM id()) AS f ON STAFF.StaffID=f.staffid) a;
+END $$
+LANGUAGE plpgsql;
+    
+
+
+
+    
